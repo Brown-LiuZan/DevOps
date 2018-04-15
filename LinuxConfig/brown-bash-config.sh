91313f45db7ed8bc3e394410a3998c8731035d38
Brown-LiuZan <brown.liuzan@outlook.com>
2018-04-15 22:17:16 +0800

    Add diretory LinuxConfig and the initial version of brown-bash-config.sh.

---
+++ LinuxConfig/brown-bash-config.sh
@@ -0,0 +1,11 @@
+# Brown's special settings of bash.
+# This file should be under the /etc/profile.d for CentOS.
+
+# Modify command promote format
+if [[ -n $PS1 ]]; then 
+    if [[ $UID -eq 0 ]]; then
+        PS1="\e[1;31m[\\u@\\H \\t \\w]\\n\\$ \e[m"
+    else
+        PS1="\e[1;36m[\\u@\\H \\t \\w]\\n\\$ \e[m"
+    fi
+fi
