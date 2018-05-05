75a63e7deda9078d697cf4a242da00b72efef2f0
Brown-LiuZan <brown.liuzan@outlook.com>
2018-05-05 10:44:09 +0800

    Fix command history browsing issue in brown-bash-config.sh

--- LinuxConfig/brown-bash-config.sh
+++ LinuxConfig/brown-bash-config.sh
@@ -1,14 +1,12 @@
-#! /usr/bin/bash
-
 # Brown's special settings of bash.
 # This file should be under the /etc/profile.d for CentOS.
-# This file is not intended to be run directly by user.
 
 # Modify command promote format
-if [[ -n $PS1 ]]; then 
+if [[ -n $PS1 ]]; then
+    #Non-printable sequences should be enclosed in \[ and \].
     if [[ $UID -eq 0 ]]; then
-        PS1="\e[1;31m[\\u@\\H \\t \\w]\\n\\$ \e[m"
+        PS1="\[\e[1;31m\][\\u@\\H \\t \\w]\\n\\$ \[\e[m\]"
     else
-        PS1="\e[1;36m[\\u@\\H \\t \\w]\\n\\$ \e[m"
+        PS1="\[\e[1;36m\][\\u@\\H \\t \\w]\\n\\$ \[\e[m\]"
     fi
 fi
