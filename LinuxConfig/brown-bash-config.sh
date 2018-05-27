645d40ee3a17f4ee2eb86427964b8e4627456eaa
Brown-LiuZan <brown.liuzan@outlook.com>
2018-05-27 20:10:22 +0800

    Add /usr/bin/bash setup step in brown-bash-config.sh

--- LinuxConfig/brown-bash-config.sh
+++ LinuxConfig/brown-bash-config.sh
@@ -1,12 +1,29 @@
-# Brown's special settings of bash.
-# This file should be under the /etc/profile.d for CentOS.
+#Brown@copyright
+#It should be under the following diretory: /etc/profile.d
+#Besides, for Ubuntu, we should append following statements to .bashrc under user home directory:
+#if [[ -f /etc/profile.d/brown-bash-config.sh ]]; then
+#    . /etc/profile.d/brown-bash-config.sh
+#fi 
 
-# Modify command promote format
+
+#Brown's special settings of bash.
+#Modify command promote format
 if [[ -n $PS1 ]]; then
     #Non-printable sequences should be enclosed in \[ and \].
     if [[ $UID -eq 0 ]]; then
         PS1="\[\e[1;31m\][\\u@\\H \\t \\w]\\n\\$ \[\e[m\]"
     else
         PS1="\[\e[1;36m\][\\u@\\H \\t \\w]\\n\\$ \[\e[m\]"
+    fi
+fi
+
+#Make sure we could always access bash from /usr/bin
+BASH_CENTOS='/usr/bin/bash'
+BASH_UBUNTU='/bin/bash'
+if [[ ! -x $BASH_CENTOS ]]; then
+    if [[ -x $BASH_UBUNTU ]]; then
+        ln -s $BASH_UBUNTU $BASH_CENTOS
+    else
+        echo 'Warning: unrecognized Linux distribution and failed to setup /usr/bin/bash' >&2
     fi
 fi
