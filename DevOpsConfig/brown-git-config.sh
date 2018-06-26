255aaf24b5fac129290991b2cc68d6bec228f48f
Brown-LiuZan <brown.liuzan@outlook.com>
2018-06-26 19:56:35 +0800

    Move brown-git-config.sh as a bash utility gitconfig.sh. Add host ip into bash prompt for brown-bash-config.sh.

--- DevOpsConfig/brown-git-config.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/bash
-
-git config --global user.name 'Brown-LiuZan'
-git config --global user.email 'brown.liuzan@outlook.com'
-git config --global push.default 'simple'
-git config --global --list
--- LinuxConfig/brown-bash-config.sh
+++ LinuxConfig/brown-bash-config.sh
@@ -10,12 +10,15 @@
 
 #Brown's special settings of bash.
 #Modify command promote format
+ip=$(ip addr | grep inet | grep brd | grep 'scope global')
+ip=${ip#*inet }
+ip=${ip%%/24*}
 if [[ -n $PS1 ]]; then
     #Non-printable sequences should be enclosed in \[ and \].
     if [[ $UID -eq 0 ]]; then
-        PS1="\[\e[1;31m\][\\u@\\H \\t \\w]\\n\\$ \[\e[m\]"
+        PS1="\[\e[1;31m\][\\u@${ip} \\t \\w]\\n\\$ \[\e[m\]"
     else
-        PS1="\[\e[1;36m\][\\u@\\H \\t \\w]\\n\\$ \[\e[m\]"
+        PS1="\[\e[1;36m\][\\u@${ip} \\t \\w]\\n\\$ \[\e[m\]"
     fi
 fi
 
