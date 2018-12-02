712cffe44148512ebb6eb6ce15eb5c5471735570
Brown-LiuZan <brown.liuzan@outlook.com>
2018-12-02 10:16:19 +0800

    Add fs travel to brown-bash-config.sh. Add taglist-config.txt.

--- brown-bash-config.sh
+++ brown-bash-config.sh
@@ -35,7 +35,32 @@ fi
 
 ulimit -c unlimited
 
+# File system travel.
 alias ls='ls --color=auto'
 alias ll='ls -l --color=auto'
 alias la='ls -a --color=auto'
+
+export LZHOME="/home/brown"
+export LZBIN="${LZHOME}/Bin"
+export LZBAK="${LZHOME}/Backups"
+export LZDWLD="${LZHOME}/Downloads"
+export LZGIT="${LZHOME}/GitHub"
+export DEVOPS="${LZGIT}/DevOps-LiuZan"
+export SHROOT="${LZGIT}/Bash-Studio-LiuZan"
+export PYROOT="${LZGIT}/Python-Studio-LiuZan"
+export CXXROOT="${LZGIT}/Cpp-Studio-LiuZan"
+export GLROOT="${LZGIT}/Go-Studio-LiuZan"
+export PATH=${PATH}:${LZBIN}
+
+alias lzhome="cd ${LZHOME}"
+alias lzbin="cd ${LZBIN}"
+alias lzbak="cd ${LZBAK}"
+alias lzdwld="cd ${LZDWLD}"
+alias lzgit="cd ${LZGIT}"
+alias devops="cd ${DEVOPS}"
+alias shroot="cd ${SHROOT}"
+alias pyroot="cd ${PYROOT}"
+alias cxxroot="cd ${CXXROOT}"
+alias cxxbuild="cd ${CXXBUILD}"
+alias glroot="cd ${GLROOT}"
 
